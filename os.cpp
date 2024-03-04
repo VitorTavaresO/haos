c7c2ab13ace2a46ae6c3a744a6cca80ba6fb43b1
VitorTavaresO <vitortavares.o55@gmail.com>
2024-03-04 14:22:19 -0300

    Started

--- os.cpp
+++ os.cpp
@@ -10,32 +10,50 @@
 #include "arq-sim.h"
 #include "os.h"
 
-
-namespace OS {
-
-// ---------------------------------------
-
-void boot (Arch::Terminal *terminal, Arch::Cpu *cpu)
+namespace OS
 {
-	terminal->println(Arch::Terminal::Type::Command, "Type commands here");
-	terminal->println(Arch::Terminal::Type::App, "Apps output here");
-	terminal->println(Arch::Terminal::Type::Kernel, "Kernel output here");
-}
+	Arch::Terminal *terminal;
 
-// ---------------------------------------
+	// ---------------------------------------
 
-void interrupt (const Arch::InterruptCode interrupt)
-{
+	void boot(Arch::Terminal *terminal, Arch::Cpu *cpu)
+	{
+		OS::terminal = terminal;
+		terminal->println(Arch::Terminal::Type::Command, "Type commands here");
+		terminal->println(Arch::Terminal::Type::App, "Apps output here");
+		terminal->println(Arch::Terminal::Type::Kernel, "Kernel output here");
+	}
 
-}
+	// ---------------------------------------
 
-// ---------------------------------------
+	void interrupt(const Arch::InterruptCode interrupt)
+	{
 
-void syscall ()
-{
+		int typed = terminal->read_typed_char();
+		if (terminal->is_alpha(typed))
+		{
+			terminal->print(Arch::Terminal::Type::Kernel, static_cast<char>(typed));
+		}
+		else if (terminal->is_num(typed))
+		{
+			terminal->print(Arch::Terminal::Type::Kernel, static_cast<char>(typed));
+		}
+		else if (terminal->is_backspace(typed))
+		{
+			terminal->print(Arch::Terminal::Type::Kernel, "\r");
+		}
+		else if (terminal->is_return(typed))
+		{
+			terminal->print(Arch::Terminal::Type::Kernel, "\n");
+		}
+	}
 
-}
+	// ---------------------------------------
 
-// ---------------------------------------
+	void syscall()
+	{
+	}
+
+	// ---------------------------------------
 
 } // end namespace OS
