aedfe19d521b28748c3a605d9d8b90ac2ac9a441
VitorTavaresO <vitortavares.o55@gmail.com>
2024-07-08 16:30:53 -0300

    New Changes on SO, almost 3 Tri working

--- os.cpp
+++ os.cpp
@@ -23,6 +23,7 @@ namespace OS
 
 	struct Process
 	{
+		std::string name;
 		uint16_t pc;
 		std::array<uint16_t, Config::nregs> registers;
 		enum class State
@@ -41,55 +42,99 @@ namespace OS
 
 	// ---------------------------------------
 
-	Process *create_process(std::string_view fname)
+	void panic(const std::string_view msg)
 	{
-		if (Lib::get_file_size_words(fname) <= Config::memsize_words)
+		terminal->println(Arch::Terminal::Type::Kernel, "Kernel Panic: " + std::string(msg));
+		cpu->turn_off();
+	}
+
+	Process *create_process(const std::string_view fname)
+	{
+		const uint32_t size = Lib::get_file_size_words(fname);
+		if (size <= Config::memsize_words)
 		{
-			static uint16_t count = 0;
 			std::vector<uint16_t> bin = Lib::load_from_disk_to_16bit_buffer(fname);
 
 			Process *process = new Process();
+			process->name = std::string(fname);
 			process->pc = 1;
 
-			if (count == 0)
+			if (idle_process_ptr == nullptr)
 			{
 				process->baser = 0;
-				count = 1;
 			}
 			else
 			{
 				process->baser = idle_process_ptr->limitr + 1;
 			}
 
-			process->limitr = (process->baser + (Lib::get_file_size_words(fname))) - 1;
+			process->limitr = (process->baser + size) - 1;
 
 			for (uint32_t i = 0; i < Config::nregs; i++)
-			{
 				process->registers[i] = 0;
-			}
 
 			process->state = Process::State::Ready;
 
 			for (uint32_t i = process->baser; i < bin.size(); i++)
-			{
 				cpu->pmem_write(i, bin[i]);
-			}
 
 			return process;
 		}
 		return nullptr;
 	}
 
+	void unschedule_process()
+	{
+		Process *process = current_process_ptr;
+
+		if (current_process_ptr == nullptr)
+			panic("No process to unschedule");
+
+		process->state = Process::State::Ready;
+		for (uint32_t i = 0; i < Config::nregs; i++)
+			process->registers[i] = cpu->get_gpr(i);
+		process->pc = cpu->get_pc();
+		current_process_ptr = nullptr;
+
+		terminal->println(Arch::Terminal::Type::Kernel, "Unschedule process: " + process->name + "\n");
+	}
+
 	void schedule_process(Process *process)
 	{
+		if (current_process_ptr != nullptr)
+			panic("Process already scheduled");
+
+		terminal->println(Arch::Terminal::Type::Kernel, "Running process: " + process->name + "\n");
+
+		process->state = Process::State::Running;
 		current_process_ptr = process;
+
 		cpu->set_pc(process->pc);
 		cpu->set_vmem_paddr_init(process->baser);
 		cpu->set_vmem_paddr_end(process->limitr);
 
 		for (uint32_t i = 0; i < Config::nregs; i++)
-		{
 			cpu->set_gpr(i, process->registers[i]);
+	}
+
+	void kill()
+	{
+		Process *process = current_process_ptr;
+
+		if (current_process_ptr != idle_process_ptr)
+		{
+			unschedule_process();
+			for (uint32_t i = process->baser; i <= process->limitr; i++)
+				cpu->pmem_write(i, 0);
+
+			schedule_process(idle_process_ptr);
+			terminal->println(Arch::Terminal::Type::Command, "Process " + process->name + " killed\n");
+			terminal->println(Arch::Terminal::Type::Kernel, "Process " + process->name + " killed\n");
+			delete process;
+		}
+		else
+		{
+			terminal->println(Arch::Terminal::Type::Command, "No process to kill");
 		}
 	}
 
@@ -112,8 +157,13 @@ namespace OS
 				if (std::filesystem::exists(filename))
 				{
 					terminal->println(Arch::Terminal::Type::Command, "Running file:" + std::string(filename) + "\n");
-					current_process_ptr = create_process(filename);
-					schedule_process(current_process_ptr);
+					if (current_process_ptr != idle_process_ptr)
+						terminal->println(Arch::Terminal::Type::Command, "File Running - Please Kill " + current_process_ptr->name + " Before Running Another File\n");
+					else
+					{
+						unschedule_process();
+						schedule_process(create_process(filename));
+					}
 				}
 				else
 				{
@@ -125,6 +175,11 @@ namespace OS
 				terminal->println(Arch::Terminal::Type::Command, "Unknown command");
 				typedCharacters.clear();
 			}
+		}
+		else if (typedCharacters == "kill")
+		{
+			kill();
+			typedCharacters.clear();
 		}
 		else
 		{
@@ -181,8 +236,6 @@ namespace OS
 		}
 	}
 
-	// ---------------------------------------
-
 	void syscall()
 	{
 		switch (cpu->get_gpr(0))
@@ -193,6 +246,9 @@ namespace OS
 		case 1:
 		{
 			uint16_t addr = cpu->get_gpr(1);
+
+			addr = current_process_ptr->baser + addr;
+
 			while (cpu->pmem_read(addr) != 0)
 			{
 
@@ -209,7 +265,5 @@ namespace OS
 			break;
 		}
 	}
-
-	// ---------------------------------------
 
 } // end namespace OS
